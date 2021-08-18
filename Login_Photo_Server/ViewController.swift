c3d6d248edac1954e90ee2d450047ced18e0a9ca
wnsgk8300 <wnsgk8300@gmail.com>
2021-08-18 10:55:20 +0900

    pod install

---
+++ Login_Photo_Server/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Login_Photo_Server
+//
+//  Created by photypeta-junha on 2021/08/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+
+target 'Login_Photo_Server' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Login_Photo_Server
+  #  pod 'Firebase/Auth'
+  #  pod 'GoogleSignIn'
+end
